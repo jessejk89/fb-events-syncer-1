6edfeea4e8b7a1e60dc84162d45484cea28d7b7f
Jesse <Jesse@Home>
2020-12-05 00:03:57 +0100

    Added production config

---
+++ prod.config.py
@@ -0,0 +1,13 @@
+fb_token = ""
+
+fb_page_id = "200346284174326"
+wp_base_url = "http://172.22.0.2/wp-json/events_api/v1"
+
+resultsEmail = ''
+
+emailServer = "smtp.gmail.com"
+emailPort = 465
+emailSender = 'xr.eventsyncer@gmail.com'
+emailPassword = ''
+
+logFile = '/var/log/fb-events-syncer/output.log'
