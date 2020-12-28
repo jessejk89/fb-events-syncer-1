9df9d3239d78d2e13d25836c4edbc23bd6edc319
Jesse <Jesse@Home>
2020-12-28 21:49:14 +0100

    Put new ip address in example config

--- example.prod.config.py
+++ example.prod.config.py
@@ -1,11 +1,11 @@
 fb_token = ""
 
 fb_page_id = "200346284174326"
-wp_base_url = "http://172.22.0.2/wp-json/events_api/v1"
+wp_base_url = "http://172.22.0.101/wp-json/events_api/v1"
 intervalHours = 4
 
-#resultsEmail = 'example@provider.nl'
-resultsEmail = ['example1@provider.nl', 'example2@provider.nl']
+resultsEmail = ['example1@provider.nl', 'example2@provider.be']
+#resultsEmail = 'example@provider.com'
 
 emailServer = "smtp.gmail.com"
 emailPort = 465
