d5c624c8243aa35de0d2982eaf6acb4c635e71d4
Jesse <Jesse@Home>
2020-12-28 01:12:21 +0100

    Added example config

---
+++ example.prod.config.py
@@ -0,0 +1,15 @@
+fb_token = ""
+
+fb_page_id = "200346284174326"
+wp_base_url = "http://172.22.0.2/wp-json/events_api/v1"
+intervalHours = 4
+
+#resultsEmail = 'example@provider.nl'
+resultsEmail = ['example1@provider.nl', 'example2@provider.nl']
+
+emailServer = "smtp.gmail.com"
+emailPort = 465
+emailSender = 'xr.eventsyncer@gmail.com'
+emailPassword = ''
+
+logFile = '/var/log/fb-events-syncer/output.log'
